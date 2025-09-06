dafeefc15b6211f8bc7a66b0f23411d99c74bd46
MieteX1 <mieszko.grempka@gmail.com>
2025-09-06 16:56:29 +0200

    Responsive

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,17 +7,18 @@ export default function HomePage() {
   return (
     <div className="relative flex h-screen overflow-hidden">
 
-      <Petal className="top-[-50px] left-[20%] animate-petal-straight" />
-      <Petal className="top-[-50px] left-[20%] animate-petal-diagonal" />
-      <Petal className="top-[-50px] right-[25%] animate-petal-straight" />
-      <Petal className="top-[-50px] right-[25%] animate-petal-lr" />
-      <Petal className="top-[-50px] right-[10%] animate-petal-straight" />
-      <Petal className="top-[-50px] left-[5%] animate-petal-lr1" />
+      {/* Płatki - na mobile mniej */}
+      <Petal className="top-[-50px] left-[20%] animate-petal-straight hidden md:block" />
+      <Petal className="top-[-50px] left-[20%] animate-petal-diagonal hidden md:block" />
+      <Petal className="top-[-50px] right-[25%] animate-petal-straight hidden md:block" />
+      <Petal className="top-[-50px] right-[25%] animate-petal-lr hidden md:block" />
+      <Petal className="top-[-50px] right-[10%] animate-petal-straight hidden md:block" />
+      <Petal className="top-[-50px] left-[5%] animate-petal-lr1"/>
       <Petal className="top-[-50px] left-[45%] animate-petal-lr1" />
       <Petal className="top-[-50px] left-[85%] animate-petal-lr1" />
 
-      {/* Lewa kolumna ze zdjęciem */}
-      <div className="h-full">
+      {/* Lewa kolumna */}
+      <div className="basis-1/3 h-full">
         <img
           src="/images/roseleft.png"
           alt="Left"
@@ -26,10 +27,10 @@ export default function HomePage() {
       </div>
 
       {/* Środkowa kolumna */}
-      <div className="flex-1 flex flex-col aboreto mt-56 items-center">
+      <div className="basis-1/3 flex flex-col aboreto mt-24 md:mt-56 items-center">
         {/* Tekst */}
         <div className="flex-1 flex flex-col justify-center items-center">
-          <h1 className="text-4xl text-white text-center">
+          <h1 className="text-xl md:text-4xl text-white text-center">
             <p>Something big is coming soon!</p>
             <br />
             <p>TBD ~this year</p>
@@ -39,20 +40,20 @@ export default function HomePage() {
         </div>
 
         {/* Progress Bar */}
-        <div className="flex flex-col items-center mb-36 space-y-4">
-          <p className="text-white text-base">Progress Bar</p>
-          <div className="w-72 border-[1px] h-4 overflow-hidden">
+        <div className="flex flex-col items-center mb-20 md:mb-36 space-y-4">
+          <p className="text-white text-sm md:text-base">Progress Bar</p>
+          <div className="w-48 md:w-72 border-[1px] h-3 md:h-4 overflow-hidden">
             <div
               className="bg-[#880808] h-full transition-all duration-500"
               style={{ width: `${progress}%` }}
             ></div>
           </div>
-          <p className="text-white">{progress}%</p>
+          <p className="text-white text-sm md:text-base">{progress}%</p>
         </div>
       </div>
 
-      {/* Prawa kolumna ze zdjęciem */}
-      <div className="h-full">
+      {/* Prawa kolumna */}
+      <div className="basis-1/3 h-full">
         <img
           src="/images/roseright.png"
           alt="Right"
